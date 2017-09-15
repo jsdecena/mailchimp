c14ebee56d47c8ccc59ccf5948cb483b64c88571
Jeff Simons Decena <jeff.decena@yahoo.com>
2017-09-15 22:21:12 +0800

    Get the error or message in the session

--- src/views/mailchimp.blade.php
+++ src/views/mailchimp.blade.php
@@ -1,5 +1,23 @@
 <h2>Subscribe to our Newsletter</h2>
-
+@if(session()->has('message'))
+    <div class="box no-border">
+        <div class="box-tools">
+            <p class="alert alert-success alert-dismissible">
+                {{ session()->get('message') }}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            </p>
+        </div>
+    </div>
+@elseif(session()->has('error'))
+    <div class="box no-border">
+        <div class="box-tools">
+            <p class="alert alert-danger alert-dismissible">
+                {{ session()->get('error') }}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            </p>
+        </div>
+    </div>
+@endif
 <form action="{{route('mailchimp.store')}}" class="form-inline" method="post">
 	<input type="hidden" name="_token" value="{{csrf_token()}}" />
 	<div class="form-group">
