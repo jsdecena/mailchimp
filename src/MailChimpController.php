d6bac0e179e21816e2e7ea75a49ce50fffee10e0
Jeff Simons Decena <jeff.decena@yahoo.com>
2017-09-15 22:38:23 +0800

    Redirect to previous page to show session message

--- src/MailChimpController.php
+++ src/MailChimpController.php
@@ -2,27 +2,34 @@
 
 namespace Jsdecena\MailChimp;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Exception;
 
-class MailChimpController
+class MailChimpController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('web');
+    }
     /**
      * @param Request $request
      * @return mixed
      */
     public function store(Request $request)
-	{
+    {
         try {
 
             $mc = new MC;
             $mc->subscribe($request->input('email'));
 
-            return $request->session()->flash('message', 'Subscription successful!');
+            $request->session()->flash('message', 'Subscription successful!');
 
         } catch (Exception $e) {
 
-            return $request->session()->flash('error', $e->getMessage());
+            $request->session()->flash('error', $e->getMessage());
         }
-	}
+
+        return redirect()->back();
+    }
 }
